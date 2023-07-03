041f524a81c21cc6d2a56b7bf44ea8701975e313
Hüseyin Karadana <huseyinkaradana35@gmail.com>
2023-07-03 14:20:40 +0300

    Save1

--- StajKisiProjesi/Program.cs
+++ StajKisiProjesi/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=PersonController1}/{action=Index}/{id?}");
 
 app.Run();
